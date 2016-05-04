dd8971527300dcfd4b8cb72179d10d24f44db55f
Chaichalit Srisawat <boywert@gmail.com>
2016-05-04 16:19:30 +0100

    quick commit

--- analysis/MergerTree.c
+++ analysis/MergerTree.c
@@ -1168,7 +1168,7 @@ void construct_filename(char filename[MAXSTRING], int32_t ifile, char infile[MAX
   suffix[ip] = '\0';
   
   sprintf(infile,"%s/%s%04"PRIi32"%s",path,prefix,ifile,suffix);
-  printf("file = %s\n",infile);
+  
 #ifdef DEBUG3
   fprintf(stderr,"\npath  =%s",path);
   fprintf(stderr,"\nbase  =%s",base);
@@ -1197,7 +1197,7 @@ int32_t count_particles_files(char filename[MAXSTRING])
     nfiles++;
     construct_filename(filename, nfiles, infile);
   }
-  
+  printf("Total file = %d\n",nfiles);
   if(nfiles > pow(10,NDIGITS)-1) {
     fprintf(stderr,"There are nfiles = %"PRIi32" but you are only using NDIGITS = %d to construct the filenames\nABORTING\n",
             nfiles,NDIGITS);
