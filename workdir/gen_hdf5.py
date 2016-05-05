a6ff57489d0a92fae99c307e577793b8e0143672
Chaichalit Srisawat <boywert@gmail.com>
2016-05-05 13:32:15 +0100

    quick commit

--- workdir/gen_hdf5.py
+++ workdir/gen_hdf5.py
@@ -31,10 +31,11 @@ def load_halocat(nsnaps,idens):
     for isnap in range(nsnaps):
         filename = outputfolder+"/snap_%03d/"%(isnap)+"/multilevels/"+prefix_template+str(overdensities[idens])+"halo.txt"
         data = numpy.loadtxt(filename)
-        halocat[firsthalo[isnap]:firsthalo[isnap]+nhalolist[isnap]]['haloID'] = data[:,0].astype(numpy.int32)
-        halocat[firsthalo[isnap]:firsthalo[isnap]+nhalolist[isnap]]['Mass'] = data[:,3]
-        for ihalo in range(nhalolist[isnap]):
-            halocat[firsthalo[isnap]+ihalo]['Pos'] = data[ihalo,5:8]
+        #halocat[firsthalo[isnap]:firsthalo[isnap]+nhalolist[isnap]]['haloID'] = data[:,0].astype(numpy.int32)
+        print data[:,0].astype(numpy.int32)
+        #halocat[firsthalo[isnap]:firsthalo[isnap]+nhalolist[isnap]]['Mass'] = data[:,3]
+        #for ihalo in range(nhalolist[isnap]):
+        #    halocat[firsthalo[isnap]+ihalo]['Pos'] = data[ihalo,5:8]
 def load_snapshot(alistfile):
     a = numpy.loadtxt(alistfile)
     nsnaps = len(a)
